704eab08b6a944a4f5b62673f8630f535a13c89e
yuanhang <15501233770@163.com>
2019-04-21 19:28:09 +0800

    update logAspect @arround

--- src/main/java/com/kingkate/busfind/aspect/LogAspect.java
+++ src/main/java/com/kingkate/busfind/aspect/LogAspect.java
@@ -2,10 +2,10 @@ package com.kingkate.busfind.aspect;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.apache.commons.lang3.time.StopWatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -13,44 +13,52 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 日志切面
+ */
 @Slf4j
 @Aspect
 @Component
 public class LogAspect {
-
-    /**
-     * 线程副本类去记录各个线程的开始时间
-     */
-    ThreadLocal<Long> startTime = new ThreadLocal<>();
 
 
     @Pointcut("execution(* com.kingkate.busfind.controller..*.*(..))")
     public void log() {
     }
 
-    @Before("log()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
-
-        startTime.set(System.currentTimeMillis());
-        // 接收到请求，记录请求内容
+    @Around("log()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        //开始计时
+        StopWatch stopWatch = StopWatch.createStarted();
+        // 记录下请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-
-        // 记录下请求内容
-        log.info("请求地址 : " + request.getRequestURL().toString());
-        log.info("请求方式 : " + request.getMethod());
-        log.info("请求IP : " + request.getRemoteAddr());
-        log.info("请求方法 : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        log.info("请求参数 : " + JSON.toJSONString(joinPoint.getArgs()));
-
-    }
-
-    @AfterReturning(returning = "ret", pointcut = "log()")
-    public void doAfterReturning(Object ret) throws Throwable {
-        // 处理完请求，返回内容
-        log.info("响应内容 : " + ret);
-        log.info("请求耗时:" + ( System.currentTimeMillis()-startTime.get() ));
-
+        Object retVal;
+        try {
+            retVal = joinPoint.proceed();
+        } catch (Throwable throwable) {
+            //响应数据执行异常时输出
+            log.info("请求地址: " + request.getRequestURL().toString());
+            log.info("请求方式: " + request.getMethod());
+            log.info("请求IP: " + request.getRemoteAddr());
+            log.info("请求方法: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+            log.info("请求参数: " + JSON.toJSONString(joinPoint.getArgs()));
+            //结束计时
+            stopWatch.stop();
+            log.info("执行时间: ms " + stopWatch.getTime());
+            throw throwable;
+        }
+        // 记录下响应内容
+        log.info("请求地址: " + request.getRequestURL().toString());
+        log.info("请求方式: " + request.getMethod());
+        log.info("请求IP: " + request.getRemoteAddr());
+        log.info("请求方法: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("请求参数: " + JSON.toJSONString(joinPoint.getArgs()));
+        //结束计时
+        stopWatch.stop();
+        log.info("执行时间: " + stopWatch.getTime()+" ms");
+        log.info("响应数据: " + JSON.toJSON(retVal));
+        return retVal;
     }
 
 }
